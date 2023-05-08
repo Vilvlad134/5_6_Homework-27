f55eeb0a3e696894faa76ae4fde36a824a9f9fb3
Vilvlad134 <vilvlad134@yandex.ru>
2023-05-08 17:17:16 +0300

    init commit

---
+++ stocks_products/logistic/views.py
@@ -0,0 +1,21 @@
+from rest_framework.filters import SearchFilter
+from rest_framework.viewsets import ModelViewSet
+
+
+from logistic.models import Product, Stock
+from logistic.serializers import ProductSerializer, StockSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['id', 'title']
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['products__id', 'products__title']
+
